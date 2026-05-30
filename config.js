fd851d61bb035433c46a63a8f1f2642c3602b4d6
heriveltogabriel <heriveltogabriel@gmail.com>
2026-05-30 00:24:40 -0300

    Rename showcase to index.html (public) and generator to gerador.html (private) and adjust URLs for custom domain

--- config.js
+++ config.js
@@ -2,9 +2,7 @@
 const CONFIG = {
     // Você pode colocar seu Client ID diretamente aqui, ou inseri-lo na tela da aplicação.
     CLIENT_ID: '', 
-    REDIRECT_URI: window.location.hostname === 'localhost' 
-        ? `http://127.0.0.1:${window.location.port || '3000'}/` 
-        : (window.location.origin + '/'), // Garante compatibilidade caso acesse por localhost ou 127.0.0.1
+    REDIRECT_URI: window.location.origin + window.location.pathname, // Resolve dinamicamente para a página atual (ex: /gerador.html)
     SCOPES: [
         'playlist-modify-public',
         'playlist-modify-private',
